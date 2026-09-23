f2e0b150fb5c400e3dbb85ec85620e83cbe596b7
piziga <azddinbahadi@gmail.com>
2026-09-23 14:33:06 +0000

    test(read files and split content into chunk)

--- src/chunker.py
+++ src/chunker.py
@@ -1,25 +1,43 @@
 from pathlib import Path
+from pydantic import BaseModel
 
+class Chunk(BaseModel):
+    content: str
+    file_path: str
+    first_character_index: int
+    last_character_index: int
 
-def chunk_text(text: str, max_size: int, path: str) -> list[dict]:
-    chunks: list[dict] = []
+def chunk_text(text: str, max_size: int, path: Path) -> list[Chunk]:
+    chunks: list[Chunk] = []
+
     for i in range(0, len(text), max_size):
         splited_text = text[i:i + max_size]
         first_character_index = i
         last_character_index = i + len(splited_text) - 1
-        chunks.append({"content": splited_text,
-                       "file_path": path,
-                       "first_character_index": first_character_index,
-                       "last_character_index": last_character_index})
+
+        chunks.append(
+                Chunk(
+                    content=splited_text,
+                    file_path=path.name,
+                    first_character_index=first_character_index,
+                    last_character_index=last_character_index)
+                )
+
     return chunks
+
+def index_directory(directory: Path) -> list[Chunk]:
+    all_chunks: list[Chunk] = []
+    for file_path in directory.glob("*.txt"):
+        with file_path.open("r") as file:
+            content = file.read()
+        all_chunks.extend(chunk_text(content, 20, file_path))
+    return all_chunks
 
 
 def main():
-    f_path = Path(
-        "../../RAG-against-the-machine/data/documents/linux.txt")
-    with f_path.open("r") as file:
-        data = file.read()
-    chunks = chunk_text(data, 5, f_path)
+    directory = Path(
+        "../../RAG-against-the-machine/data/documents")
+    chunks = index_directory(directory)
     print("Text to chinks: \n", chunks)
 
 
