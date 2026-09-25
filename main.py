f8bb9ffb5695c96ced75b2f5bb760d8e0f3ded83
piziga <azddinbahadi@gmail.com>
2026-09-25 17:02:58 +0000

    start building and test

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from src.models.models import UnansweredQuestion
+
+
 def main():
-    print("Hello from rag-against-the-machine!")
+    q1 = UnansweredQuestion(question="Question 1")
+    q2 = UnansweredQuestion(question="Question 2")
+    print(q1.question)
+    print(q2.question)
+    print(q1.question_id)
+    print(q2.question_id)
+    print(q1.question_id == q2.question_id)
+    #print("Hello from rag-against-the-machine!")
 
 
 if __name__ == "__main__":
